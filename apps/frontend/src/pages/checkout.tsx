d4b8dbef2490d2feb81112961e04d96823a2fd22
Arsen1987144 <Arsen1987144@icloud.com>
2025-08-31 03:38:49 -0700

    Create checkout page

---
+++ apps/frontend/src/pages/checkout.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
+export default function CheckoutPage() {
+  return (
+    <Layout>
+      <Head>
+        <title>Оформление заказа – MarketDemo</title>
+      </Head>
+      <h1>Оформление заказа</h1>
+      <p>Функциональность оформления заказа будет добавлена позже.</p>
+    </Layout>
+  );
+}
